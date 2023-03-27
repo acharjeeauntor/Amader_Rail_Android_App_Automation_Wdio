8acfb7b7f6babf5d0b6515fbac82dcc29ebdb7f2
Auntor Acharja <acharjeeauntor@gmail.com>
2023-03-27 23:26:39 +0600

    Update info

--- config/wdio.localdevicebs.conf.js
+++ config/wdio.localdevicebs.conf.js
@@ -1,54 +1,54 @@
-const config = require("./wdio.shared.conf")
-require('dotenv').config({
-    path:`.env.test`,
-   override:true
-})
+// const config = require("./wdio.shared.conf")
+// require('dotenv').config({
+//     path:`.env.test`,
+//    override:true
+// })
 
-config.user = process.env.BS_USER
-config.key = process.env.BS_KEY
+// config.user = process.env.BS_USER
+// config.key = process.env.BS_KEY
 
-config.specs = [
+// config.specs = [
 
-    './test/specs/landing/landing.spec.js',
-    './test/specs/account/registration.spec.js',
-    './test/specs/home/home.spec.js',
-    './test/specs/search/search.spec.js',
-    './test/specs/traintimeline/traintimeline.spec.js',
-    './test/specs/ticketprice/ticketprice.spec.js',
-    './test/specs/traintracking/traintracking.spec.js',
-    './test/specs/mobileticket/mobileticket.spec.js',
-    './test/specs/subscribe/subscribe.spec.js'
+//     './test/specs/landing/landing.spec.js',
+//     './test/specs/account/registration.spec.js',
+//     './test/specs/home/home.spec.js',
+//     './test/specs/search/search.spec.js',
+//     './test/specs/traintimeline/traintimeline.spec.js',
+//     './test/specs/ticketprice/ticketprice.spec.js',
+//     './test/specs/traintracking/traintracking.spec.js',
+//     './test/specs/mobileticket/mobileticket.spec.js',
+//     './test/specs/subscribe/subscribe.spec.js'
 
-],
-    config.suites = {
-        account: [
+// ],
+//     config.suites = {
+//         account: [
 
-            './test/specs/landing/landing.spec.js',
-            './test/specs/account/registration.spec.js',
-            './test/specs/home/home.spec.js'
+//             './test/specs/landing/landing.spec.js',
+//             './test/specs/account/registration.spec.js',
+//             './test/specs/home/home.spec.js'
 
-        ],
-        basicTest: [
+//         ],
+//         basicTest: [
 
-            './test/specs/search/search.spec.js',
-            './test/specs/traintimeline/traintimeline.spec.js',
-            './test/specs/ticketprice/ticketprice.spec.js',
-            './test/specs/traintracking/traintracking.spec.js'
-        ]
-    },
+//             './test/specs/search/search.spec.js',
+//             './test/specs/traintimeline/traintimeline.spec.js',
+//             './test/specs/ticketprice/ticketprice.spec.js',
+//             './test/specs/traintracking/traintracking.spec.js'
+//         ]
+//     },
 
 
-    config.capabilities = [
-        {
-            platformName: "Android",
-            "appium:platformVersion": "9.0",
-            "appium:deviceName": "Google Pixel 3a",
-            "appium:automationName": "UIAutomator2",
-            "appium:app": "bs://0d1db8abc756cd07d625628818d32def1f5b5997",
-            "appium:autoGrantPermissions": true
-        }
-    ]
+//     config.capabilities = [
+//         {
+//             platformName: "Android",
+//             "appium:platformVersion": "9.0",
+//             "appium:deviceName": "Google Pixel 3a",
+//             "appium:automationName": "UIAutomator2",
+//             "appium:app": "bs://0d1db8abc756cd07d625628818d32def1f5b5997",
+//             "appium:autoGrantPermissions": true
+//         }
+//     ]
 
-config.services = ['browserstack'];
+// config.services = ['browserstack'];
 
-exports.config = config;
+// exports.config = config;
